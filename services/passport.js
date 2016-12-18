c31bf883a2f07f1100350f760d2ac20e3f6107be
Jose Esquivel <jose.esquivel@socialreality.com>
2016-12-18 13:18:39 -0800

    Finish auth server

---
+++ services/passport.js
@@ -0,0 +1,56 @@
+const passport = require('passport');
+const User = require('../models/user');
+const config = require('../config');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJWt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+//Create local strategy
+const localOptions = {usernameField: 'email'};
+const localLogin = new LocalStrategy(localOptions, function (email, password, done) {
+    User.findOne({email: email}, function (err, user) {
+       if(err){return done(err);}
+       if(!user){return done(null, false);}
+
+       user.comparePasword(password, function (err, isMatch) {
+           if(err){
+               return done(err);
+           }
+           if(!isMatch){
+               return done(null, false);
+           }
+
+           return done(null, user);
+       })
+    });
+});
+
+//Setup options for JWT Strategy
+const jwtOptions = {
+    jwtFromRequest: ExtractJWt.fromHeader('authorization'),
+    secretOrKey: config.secret
+};
+
+//Create JWT strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function (payload, done) {
+    // see if the user id in the payload exists in our database
+    // If it does, call 'done' with that user
+    //otherwise, call done without a user object
+
+    User.findById(payload.sub, function (err, user) {
+        if (err) {
+            return done(err, false);
+        }
+
+        if (user) {
+            done(null, user);
+        } else {
+            done(null, false);
+        }
+    })
+});
+
+
+//Tell passport to use this strategy
+passport.use(jwtLogin);
+passport.use(localLogin);
